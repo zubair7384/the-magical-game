be94221c9bd1fe7e15a687ab91805bd7df89ea2b
zubair7384 <zubairiqbal@uconnect.pk>
2018-12-19 00:34:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let firstStep = alert("Dil ma koe number rakhoo!");
+let secondStep = alert(
+  "Dost se b otnay paysay lao jitnay ap ne dil ma rakhy han."
+);
+var myArray = [10, 20, 30, 40, 50, 60];
+var randomNum = myArray[Math.floor(Math.random() * myArray.length)];
+let thirdStep = alert("Ma nay aap ko " + randomNum + " rupay deyai.");
+let fourthStep = alert(
+  "saare raqam ko jama kar k us ka aaada hissa kisi ghareeb ko daydo ya J.K ma khusoose mehmaani karoo!"
+);
+let fifthStep = alert("Aap ne dost se jitnay payse mangy thay wo wapis karoo.");
+var result = randomNum / 2;
+let sixthStep = alert("Ab aap k pass " + result + " rupay bachay hongay.");
